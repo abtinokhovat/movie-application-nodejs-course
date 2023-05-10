79fdb34f0a1cd39c05c419011477939a0e73f6d4
abtinokhovat <abtinokhovat@gmail.com>
2023-05-10 13:51:20 +0330

    update routes

--- routes/movies.js
+++ routes/movies.js
@@ -2,10 +2,4 @@ const express = require("express");
 const router = express.Router();
 const Movie = require("../models/movie");
 
-// Get all movies
-router.get("/", async (req, res) => {
-  const movies = Movie.find().exec();
-  res.send(movies);
-});
-
 module.exports = router;
